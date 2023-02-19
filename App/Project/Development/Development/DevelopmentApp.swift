ee7459115dfc56add471179cd41fa3b59212dfe1
j.kwon <j.kwon@fenrir.co.jp>
2023-02-19 10:41:27 +0900

    add Development Project

---
+++ App/Project/Development/Development/DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentApp.swift
+//  Development
+//
+//  Created by クォン ジュンヒョク on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
