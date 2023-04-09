3fa7d13c506d896dd3a9ca32e5fd8531e6b12e6b
j.kwon <j.kwon@fenrir.co.jp>
2023-04-09 09:54:32 +0900

    ThemeのPreset追加

--- App/Anniversaries/Sources/Theme/Core/Theme.swift
+++ App/Anniversaries/Sources/Theme/Core/Theme.swift
@@ -2,6 +2,7 @@
 //  Created by Maharjan Binish on 2023/03/19.
 //
 
+import AppUI
 import ComposableArchitecture
 import Foundation
 import SwiftUI
@@ -46,16 +47,45 @@ public struct Theme: ReducerProtocol {
 extension Theme {
     public enum Preset: Int {
         case `default`
+        case midnightSky
+        case sunriseGlow
+        case forestWalk
+        case cherryBlossom
+        case oceanBreeze
+
+
 
         public var backgroundColor: Color {
             switch self {
-            case .default: return .white
+            case .default:
+                return Asset.Colors.white.swiftUIColor
+            case .midnightSky:
+                return Asset.Colors.darkBlue.swiftUIColor
+            case .sunriseGlow:
+                return Asset.Colors.orange.swiftUIColor
+            case .forestWalk:
+                return Asset.Colors.forestGreen.swiftUIColor
+            case .cherryBlossom:
+                return Asset.Colors.softRed.swiftUIColor
+            case .oceanBreeze:
+                return Asset.Colors.skyBlue.swiftUIColor
             }
         }
 
         public var foregroundColor: Color {
             switch self {
-            case .default: return .black
+            case .default:
+                return Asset.Colors.black.swiftUIColor
+            case .midnightSky:
+                return Asset.Colors.lightGray.swiftUIColor
+            case .sunriseGlow:
+                return Asset.Colors.darkBlue.swiftUIColor
+            case .forestWalk:
+                return Asset.Colors.white.swiftUIColor
+            case .cherryBlossom:
+                return Asset.Colors.lightYellow.swiftUIColor
+            case .oceanBreeze:
+                return Asset.Colors.white.swiftUIColor
             }
         }
     }
