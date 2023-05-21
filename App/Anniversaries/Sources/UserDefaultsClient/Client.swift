d145c7c90c93a56ad93bc40723d5da6fefc46b89
Maharjan Binish <binish@fenrir.co.jp>
2023-05-21 11:06:36 +0900

    added sort function to userdefaults

--- App/Anniversaries/Sources/UserDefaultsClient/Client.swift
+++ App/Anniversaries/Sources/UserDefaultsClient/Client.swift
@@ -2,12 +2,17 @@
 //  Created by Maharjan Binish on 2023/03/19.
 //
 
+import Core
 import Foundation
 import Dependencies
 
 public struct UserDefaultsClient {
     public var currentTheme: () -> Int
     public var setCurrentTheme: (Int) -> Void
+    public var currentAnniversariesSort: () -> Sort
+    public var setCurrentAnniversariesSort: (Sort) -> Void
+    public var currentAnniversariesSortOrder: () -> Core.SortOrder
+    public var setCurrentAnniversariesSortOrder: (Core.SortOrder) -> Void
 }
 
 extension DependencyValues {
@@ -21,11 +26,19 @@ extension DependencyValues {
 extension UserDefaultsClient: TestDependencyKey {
     public static let testValue = Self(
         currentTheme: unimplemented(),
-        setCurrentTheme: unimplemented()
+        setCurrentTheme: unimplemented(),
+        currentAnniversariesSort: unimplemented(),
+        setCurrentAnniversariesSort: unimplemented(),
+        currentAnniversariesSortOrder: unimplemented(),
+        setCurrentAnniversariesSortOrder: unimplemented()
     )
 
     public static let previewValue = Self(
         currentTheme: { 0 },
-        setCurrentTheme: { _ in }
+        setCurrentTheme: { _ in },
+        currentAnniversariesSort: { .date },
+        setCurrentAnniversariesSort: { _ in },
+        currentAnniversariesSortOrder: { .newest },
+        setCurrentAnniversariesSortOrder: { _ in }
     )
 }
--- App/Anniversaries/Sources/UserDefaultsClient/Live.swift
+++ App/Anniversaries/Sources/UserDefaultsClient/Live.swift
@@ -2,11 +2,14 @@
 //  Created by Maharjan Binish on 2023/03/19.
 //
 
+import Core
 import Foundation
 import Dependencies
 
 private enum Keys {
     static let currentTheme = "currentTheme"
+    static let currentAnniversariesSort = "currentAnniversariesSort"
+    static let currentAnniversariesSortOrder = "currentAnniversariesSortOrder"
 }
 
 // - MARK: Dependency (liveValue)
@@ -16,14 +19,72 @@ extension UserDefaultsClient: DependencyKey {
 
 // MARK: - Live Implementation
 extension UserDefaultsClient {
-    public static func live(userDefaults: UserDefaults) -> Self {
+    public static func live(
+        userDefaults: UserDefaults,
+        jsonDecoder: JSONDecoder = JSONDecoder(),
+        jsonEncoder: JSONEncoder = JSONEncoder()
+    ) -> Self {
         Self(
             currentTheme: {
                 userDefaults.integer(forKey: Keys.currentTheme)
             },
             setCurrentTheme: {
                 userDefaults.set($0, forKey: Keys.currentTheme)
+            },
+            currentAnniversariesSort: {
+                guard let data = userDefaults.data(forKey: Keys.currentAnniversariesSort),
+                      let sort = try? jsonDecoder.decode(Sort.self, from: data) else {
+                    return .date
+                }
+
+                return sort
+            },
+            setCurrentAnniversariesSort: { sort in
+                guard let data = try? jsonEncoder.encode(sort) else {
+                    assertionFailure("Failed UserDefaults.sort.set")
+                    return
+                }
+
+                userDefaults.set(data, forKey: Keys.currentAnniversariesSort)
+            },
+            currentAnniversariesSortOrder: {
+                guard let data = userDefaults.data(forKey: Keys.currentAnniversariesSortOrder),
+                      let sortOrder = try? jsonDecoder.decode(Core.SortOrder.self, from: data)  else {
+                    return .newest
+                }
+
+                return sortOrder
+            },
+            setCurrentAnniversariesSortOrder: { sortOrder in
+                guard let data = try? jsonEncoder.encode(sortOrder) else {
+                    assertionFailure("Failed UserDefaults.sort.set")
+                    return
+                }
+
+                userDefaults.set(data, forKey: Keys.currentAnniversariesSortOrder)
             }
         )
     }
 }
+
+//
+//get {
+//    guard let data = data(forKey: Key.myShops.rawValue) else {
+//        return []
+//    }
+//    let jsonDecoder = JSONDecoder()
+//    do {
+//        return try jsonDecoder.decode([ResponseValues.B01_B02_B03_B04MyShopList.MyShop].self, from: data)
+//    } catch {
+//        assertionFailure("Failed UserDefaults.myShops.get: \(error)")
+//        return []
+//    }
+//}
+//set {
+//    let jsonEncoder = JSONEncoder()
+//    do {
+//        set(try jsonEncoder.encode(newValue), forKey: Key.myShops.rawValue)
+//    } catch {
+//        assertionFailure("Failed UserDefaults.myShops.set: \(error)")
+//    }
+//}
