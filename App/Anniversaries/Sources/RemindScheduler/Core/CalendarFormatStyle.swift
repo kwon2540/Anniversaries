28323f8d8fb597bc890451c5efabaed5be2d3148
Maharjan Binish <binish@fenrir.co.jp>
2023-08-13 11:27:34 +0900

    rename calendarDateStyle to calendarFormatStyle

--- App/Anniversaries/Sources/RemindScheduler/Core/CalendarFormatStyle.swift
+++ App/Anniversaries/Sources/RemindScheduler/Core/CalendarFormatStyle.swift
@@ -18,3 +18,17 @@ struct CalendarDateStyle: FormatStyle {
 extension FormatStyle where Self == CalendarDateStyle {
     static var calendarDate: CalendarDateStyle { .init() }
 }
+
+struct CalendarTimeStyle: FormatStyle {
+    public func format(_ value: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .none
+
+        return formatter.string(from: value)
+    }
+}
+
+extension FormatStyle where Self == CalendarTimeStyle {
+    static var calendarTime: CalendarTimeStyle { .init() }
+}
