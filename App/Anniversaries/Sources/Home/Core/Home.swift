947f5e93fe933591bf31f9da122ceb2d90941dd1
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-01-28 11:54:55 +0900

    fix the delete anniversary animation bug

--- App/Anniversaries/Sources/Home/Core/Home.swift
+++ App/Anniversaries/Sources/Home/Core/Home.swift
@@ -15,17 +15,16 @@ import SwiftUI
 public struct Home {
     public struct State: Equatable {
         public init() {}
-
+        
         @PresentationState var destination: Destination.State?
         @BindingState var editMode: EditMode = .inactive
-
+        
         var anniversaries: [Anniversary] = []
         var groupedAnniversariesList: [GroupedAnniversaries] = []
         var currentSort: Sort.Kind = .defaultCategory
         var currentSortOrder: Sort.Order = .ascending
-        var isUpdateGroupedAnniversaries = true
     }
-
+    
     public enum Action: Equatable, BindableAction {
         case binding(BindingAction<State>)
         
@@ -43,12 +42,13 @@ public struct Home {
         case onDeleteAnniversary(Anniversary)
         case deleteAnniversary(TaskResult<VoidSuccess>)
     }
-
+    
     public init() {}
-
+    
     @Dependency(\.userDefaultsClient) private var userDefaultClient
     @Dependency(\.anniversaryDataClient) private var anniversaryDataClient
-
+    @Dependency(\.continuousClock) private var clock
+    
     public var body: some ReducerOf<Self> {
         BindingReducer()
         Reduce<State, Action> { state, action in
@@ -56,9 +56,9 @@ public struct Home {
             case .onAppear where state.anniversaries.isEmpty:
                 state.currentSort = userDefaultClient.currentAnniversariesSort()
                 state.currentSortOrder = userDefaultClient.currentAnniversariesSortOrder()
-
+                
                 return .send(.fetchAnniversaries)
-
+                
             case .fetchAnniversaries:
                 return .run { send in
                     await send(
@@ -69,47 +69,47 @@ public struct Home {
                         )
                     )
                 }
-
+                
             case .anniversariesResponse(.success(let anniversaries)):
                 state.anniversaries = anniversaries
                 
-                if state.isUpdateGroupedAnniversaries {
-                    return .send(.sortAnniversaries)
-                }
-
+                return .send(.sortAnniversaries)
+                
             case .anniversariesResponse(.failure(let error)):
                 assertionFailure(error.localizedDescription)
                 state.destination = .alert(
                     AlertState(title: TextState(#localized("Failed to load anniversary")))
                 )
-
+                
             case .sortAnniversaries:
                 state.groupedAnniversariesList = state.anniversaries.sorted(by: state.currentSort, order: state.currentSortOrder)
-
+                
             case .editButtonTapped:
                 state.editMode = .active
                 
             case .editDoneButtonTapped:
                 state.editMode = .inactive
-
+                
             case .sortByButtonTapped(let sort):
                 state.currentSort = sort
                 userDefaultClient.setCurrentAnniversariesSort(sort)
                 return .send(.sortAnniversaries)
-
+                
             case .sortOrderButtonTapped(let sortOrder):
                 state.currentSortOrder = sortOrder
                 userDefaultClient.setCurrentAnniversariesSortOrder(sortOrder)
                 return .send(.sortAnniversaries)
-
+                
             case .addButtonTapped:
                 state.destination = .add(.init(mode: .add))
-
+                
             case .anniversaryTapped(let anniversary):
                 state.destination = .edit(.init(mode: .edit(anniversary)))
                 
             case .onDeleteAnniversary(let anniversary):
                 return .run { send in
+                    // Wait for delete animation to complete
+                    try await clock.sleep(for: .seconds(0.3))
                     await send(
                         .deleteAnniversary(
                             TaskResult {
@@ -121,21 +121,26 @@ public struct Home {
                 }
                 
             case .deleteAnniversary(.success):
-                // While deleting, if groupedAnniversaries is updated then delete animation is not shown
-                state.isUpdateGroupedAnniversaries = false
                 return .send(.fetchAnniversaries)
                 
             case .deleteAnniversary(.failure):
                 state.destination = .alert(
-                    AlertState(title: TextState(#localized("Failed to delete anniversary")))
-                )      
-                return .send(.fetchAnniversaries)
-
+                    AlertState {
+                        TextState(#localized("Failed to delete anniversary"))
+                    } actions: {
+                        ButtonState(action: .onDismissed) { TextState(#localized("OK")) }
+                    }
+                )
+                
             case .destination(.presented(.add(.delegate(.saveAnniversarySuccessful)))),
                     .destination(.presented(.edit(.delegate(.saveAnniversarySuccessful)))):
-                state.isUpdateGroupedAnniversaries = true
                 return .send(.fetchAnniversaries)
-
+                
+            case .destination(.presented(.alert(.onDismissed))):
+                // Clearing grouped anniversary, so that view will re-render
+                state.groupedAnniversariesList = []
+                return .send(.fetchAnniversaries)
+                
             case .binding, .destination, .onAppear:
                 break
             }
@@ -156,15 +161,16 @@ extension Home {
             case edit(AddAndEdit.State)
             case alert(AlertState<Action.Alert>)
         }
-
+        
         public enum Action: Equatable {
             public enum Alert: Equatable {
+                case onDismissed
             }
             case add(AddAndEdit.Action)
             case edit(AddAndEdit.Action)
             case alert(Alert)
         }
-
+        
         public var body: some ReducerOf<Self> {
             Scope(state: \.add, action: \.add, child: AddAndEdit.init)
             Scope(state: \.edit, action: \.edit, child: AddAndEdit.init)
