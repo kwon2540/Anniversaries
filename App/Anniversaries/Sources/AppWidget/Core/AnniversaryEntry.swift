aa724275c143313c06ca7293abd4827e4c7a10a6
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-05-19 12:08:51 +0900

    Create AnniversaryEntry

---
+++ App/Anniversaries/Sources/AppWidget/Core/AnniversaryEntry.swift
@@ -0,0 +1,46 @@
+//
+//  Created by マハルジャン ビニシュ on 2024/05/19.
+//
+
+import Foundation
+import SwiftDataClient
+import WidgetKit
+import CoreKit
+
+struct AnniversaryEntry: TimelineEntry {
+    var date: Date
+    var kind: AnniversaryKind
+    var title: String
+    var name: String
+    var targetDate: Date
+    var isEmpty: Bool
+    
+    var imageName: String {
+        return switch kind {
+        case .birth: "icon_birthday"
+        case .remembrance: "icon_remembrance"
+        case .others: "icon_others"
+        }
+    }
+}
+
+extension AnniversaryEntry {
+    init(date: Date, anniversary: Anniversary) {
+        self.date = date
+        self.kind = anniversary.kind
+        self.title = anniversary.kind == .others ? anniversary.othersTitle : anniversary.kind.title
+        self.name = anniversary.name
+        self.targetDate = anniversary.date
+        self.isEmpty = false
+    }
+}
+
+extension Date {
+    var daysRemaining: String {
+        let calendar = Calendar.current
+        let now = calendar.startOfDay(for: .now)
+        let targetDate = calendar.startOfDay(for: self)
+        let components = calendar.dateComponents([.day], from: now, to: targetDate)
+        return String(components.day ?? 0)
+    }
+}
