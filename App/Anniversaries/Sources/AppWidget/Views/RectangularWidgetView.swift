9d41d1ba608bb11f5a385cde2b33c6299c9d145c
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-06-30 11:37:05 +0900

    fix rectangular widget ui

--- App/Anniversaries/Sources/AppWidget/Views/RectangularWidgetView.swift
+++ App/Anniversaries/Sources/AppWidget/Views/RectangularWidgetView.swift
@@ -13,18 +13,30 @@ struct RectangularWidgetView: View {
         if entry.isEmpty {
             Text("No Anniversary")
         } else {
-            VStack(alignment: .leading) {
-                HStack {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(spacing: 4) {
                     #image(entry.imageName)
                         .resizable()
                         .frame(width: 28, height: 28)
-                    Text("\(entry.targetDate.daysRemaining) Days Left")
-                        .font(.system(size: 18).bold())
-                        .minimumScaleFactor(0.5)
+                    
+                    HStack(alignment: .firstTextBaseline, spacing: 2) {
+                        Text("\(entry.daysRemaining)")
+                            .font(.system(size: 18))
+                            .bold()
+                            .minimumScaleFactor(0.5)
+                        if !entry.anniversaryDate.hasSameMonthAndDayAsToday {
+                            Text(#localized("Days Left"))
+                                .bold()
+                        }
+                    }
                 }
                 VStack(alignment: .center) {
                     Text(entry.name)
-                    Text(entry.anniversaryDate.formatted(date: .abbreviated, time: .omitted))
+                        .font(.system(size: 20))
+                        .bold()
+                    Text(entry.anniversaryDate.formatted(.widgetDate))
+                        .font(.system(size: 20))
+                        .bold()
                 }
                 .frame(maxWidth: .infinity)
                 .minimumScaleFactor(0.5)
