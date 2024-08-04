6cfeb0f19346cac4e6588252d552188bf85472c0
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-08-04 09:26:02 +0900

    update swift syntax to 510.0.3

--- App/Anniversaries/Package.swift
+++ App/Anniversaries/Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.12.1"),
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "510.0.3"),
     ],
     targets: [
         .target(
